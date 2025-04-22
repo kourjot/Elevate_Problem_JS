812f7abf736825253e8fd9e9d2c0a2f02d386a0a
kourjot <pkour6188@gmail.com>
2025-04-23 00:20:57 +0530

    Dsa

---
+++ 22-04-25/dsa/q3.js
@@ -0,0 +1,16 @@
+var search = function(nums, target) {
+    let i=0
+    let j=nums.length-1
+    while(i<=j){
+        let mid=Math.floor((i+j)/2)
+        if (nums[mid]==target){
+            return mid
+        }else if(nums[mid]>target){
+            j=mid-1
+        }else{
+            i=mid+1
+        }
+    }
+    return -1
+    
+};
